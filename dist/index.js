7f2980491fefa7d51592d535e700af1021e27ced
mvecha <mvecha@zscaler.com>
2022-02-18 16:24:29 +0530

    Including github_sarif

--- dist/index.js
+++ dist/index.js
@@ -17741,7 +17741,8 @@ const executeScan = function () {
                 if (stderr) {
                     console.log(stderr);
                 }
-                if (outputFormat.startsWith("sarif") || outputFormat.endsWith("sarif")) {
+                if (outputFormat.startsWith("sarif") || outputFormat.endsWith("sarif") ||
+                    outputFormat.startsWith("github_sarif") || outputFormat.endsWith("github_sarif")) {
                     core.setOutput('sarif_file_path', process.cwd() + '/result.sarif')
                 }
                 if (outputFormat.startsWith("json")) {
--- src/zscanner/scanner.js
+++ src/zscanner/scanner.js
@@ -83,7 +83,8 @@ const executeScan = function () {
                 if (stderr) {
                     console.log(stderr);
                 }
-                if (outputFormat.startsWith("sarif") || outputFormat.endsWith("sarif")) {
+                if (outputFormat.startsWith("sarif") || outputFormat.endsWith("sarif") ||
+                    outputFormat.startsWith("github_sarif") || outputFormat.endsWith("github_sarif")) {
                     core.setOutput('sarif_file_path', process.cwd() + '/result.sarif')
                 }
                 if (outputFormat.startsWith("json")) {
