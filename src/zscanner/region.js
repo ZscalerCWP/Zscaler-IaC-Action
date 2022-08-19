85a70dde9bf4bf79b873b6a5244e645fe1f760eb
dibyah <96776869+dibyah@users.noreply.github.com>
2022-08-19 13:08:00 +0530

    Add EU region details

--- src/zscanner/region.js
+++ src/zscanner/region.js
@@ -3,6 +3,10 @@ var region = {
         'auth_url' : 'https://auth.us.zpccloud.net',
         'api_url' : 'https://api.zpccloud.net'
     },
+    'EU' : {
+        'auth_url' : 'https://auth.eu.zpccloud.net'  ,
+        'api_url' : 'https://api.eu.zpccloud.net',
+    },
     'CUSTOM' : {
         'auth_url' : process.env.AUTH_URL,
         'api_url' : process.env.API_URL
